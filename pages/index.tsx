b1a1d83ab64544e94cb8b05a48bd79c027719d22
C4I0 <107209603+caio-farias21@users.noreply.github.com>
2023-06-10 15:28:18 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,14 @@ export default function Home() {
       <div className="container py-5 mb-5">
         <h1 className="fw-bold text-green text-md-start text-center">Sobre</h1>
         <p className="text-wrap fw-medium text-md-start text-center">
-          Este site tem como objetivo, monitorar e informar a população sobre o
-          progresso do processo de arborização da cidade de Campos Belos -
-          Goiás.
+          Este site tem como objetivo, monitorar e informar a população local
+          sobre o progresso do processo de arborização da cidade de Campos Belos
+          - Goiás.
+          <img
+            className="img-fluid ms-2"
+            style={{ width: "1.5em", borderRadius: "2px" }}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_Goi%C3%A1s.svg/1280px-Flag_of_Goi%C3%A1s.svg.png"
+          ></img>
         </p>
       </div>
     </>
